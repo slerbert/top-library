2b107b01a92fd3961aedeff6a1bd30c07078add6
slerbert <156713928+slerbert@users.noreply.github.com>
2024-10-02 04:29:06 +1000

    Refactor script.js
    Abstracted away card initialization from displayBooks function.
    Added more comments to clarify certain parts throughout script.js.
    Changed some variable/function names to clearer alternatives (e.g. "showButton" to "newBookBtn")

--- script.js
+++ script.js
@@ -3,91 +3,109 @@ function displayBooks() {
     bookContainer.replaceChildren();
 
     for (let i = 0; i < myLibrary.length; i++) {
+        // Get book from library and initalize blank card
         const currentBook = myLibrary[i];
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.setAttribute('data-index', i);
-
-        const cardHeader = document.createElement('div');
-        cardHeader.classList.add('header');
-        cardHeader.textContent = currentBook.title;
-
-        const crossImg = document.createElement('img');
-        crossImg.setAttribute('src', 'cross.svg');
-        crossImg.setAttribute('height', '800px');
-        crossImg.setAttribute('width', '800px');
-        crossImg.setAttribute('alt', 'Remove book');
-        cardHeader.appendChild(crossImg);
-
-        const cardBody = document.createElement('div');
-        cardBody.classList.add('card-body');
-
-        const author = document.createElement('p');
-        author.textContent = `Author: ${currentBook.author}`;
-        cardBody.appendChild(author);
-
-        const pages = document.createElement('p');
-        pages.textContent = `Pages: ${currentBook.pages}`;
-        cardBody.appendChild(pages);
-
-        const read = document.createElement('p');
-        read.textContent = `Read? ${currentBook.read === true ? 'Yes' : 'No'}`;
-        cardBody.appendChild(read);
-
-        const cardFooter = document.createElement('div');
-        cardFooter.classList.add('card-footer');
-
-        const toggleReadBtn = document.createElement('button');
-        toggleReadBtn.setAttribute('type', 'button');
+        const card = initializeCard(i);
         
-        if (currentBook.read === true) {
-            toggleReadBtn.textContent = 'Mark as Unread';
-        } else {
+        // Populate card with data from currentBook
+        card.querySelector('.header').prepend(currentBook.title);
+        card.querySelector('#author').textContent = `Author: ${currentBook.author}`;
+        card.querySelector('#pages').textContent = `Pages: ${currentBook.pages}`;
+        card.querySelector('#read').textContent = `Read? ${currentBook.read === true ? 'Yes' : 'No'}`;
+
+        // Toggle appearance and text of button in card footer depending on read status
+        const toggleReadBtn = card.querySelector('button');
+        if (currentBook.read === false) {
             toggleReadBtn.textContent = 'Mark as Read';
             toggleReadBtn.classList.toggle('toggle-read');
         }
-
-        cardFooter.appendChild(toggleReadBtn);
-        
-        card.appendChild(cardHeader);
-        card.appendChild(cardBody);
-        card.appendChild(cardFooter);
 
         bookContainer.appendChild(card);
     }
 }
 
+function initializeCard(index) {
+    // Creates card element and populates card with children
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.setAttribute('data-index', index);
+
+    const cardHeader = document.createElement('div');
+    const cardBody = document.createElement('div');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const read = document.createElement('p');
+    const cardFooter = document.createElement('div');
+    const toggleReadBtn = document.createElement('button');    
+    const crossImg = new Image();
+
+    crossImg.setAttribute('src', 'cross.svg');
+    crossImg.setAttribute('height', '20px');
+    crossImg.setAttribute('width', '20px');
+    crossImg.setAttribute('alt', 'Remove book');
+
+    crossImg.id = 'removeBook';
+    author.id = 'author';
+    pages.id = 'pages';
+    read.id = 'read';
+    toggleReadBtn.id = 'toggleRead';
+
+    toggleReadBtn.setAttribute('type', 'button');
+    toggleReadBtn.textContent = 'Mark as Unread';
+
+    cardHeader.classList.add('header');
+    cardBody.classList.add('card-body');
+    cardFooter.classList.add('card-footer');
+    
+    
+    cardHeader.appendChild(crossImg);
+    cardBody.appendChild(author);
+    cardBody.appendChild(pages);
+    cardBody.appendChild(read);
+    cardFooter.appendChild(toggleReadBtn);
+    
+    card.appendChild(cardHeader);
+    card.appendChild(cardBody);
+    card.appendChild(cardFooter);
+
+    return card;
+}
+
 const bookContainer = document.querySelector('.books');
-const showButton = document.querySelector('#showDialog');
+const newBookBtn = document.querySelector('#newBook');
 const dialog = document.querySelector('#addBookDialog');
 const dialogForm = dialog.querySelector('form');
 const confirmBtn = dialog.querySelector('#confirmBtn');
 const cancelBtn = dialog.querySelector('#cancelBtn');
 
 bookContainer.addEventListener('click', (e) => {
-    const targetTagName = e.target.tagName;
-    const bookIndex = e.target.parentElement.parentElement.getAttribute('data-index');
+    // Get index of the parent card
+    const bookIndex = e.target.closest('.card').getAttribute('data-index');
     
-    if (targetTagName === 'IMG') {
-        // Remove item at bookIndex from myLibrary array
+    if (e.target.id === 'removeBook') {
+        // Remove item at bookIndex from myLibrary array if element clicked is the cross
         removeBookFromLibrary(bookIndex);
         displayBooks();
-    } else if (targetTagName === 'BUTTON') {
+    } else if (e.target.id === 'toggleRead') {
+        // Toggle read status of book if element clicked is the button in the footer of the card
         myLibrary[bookIndex].toggleReadStatus();
         displayBooks();
     }
 });
 
-showButton.addEventListener('click', () => {
+newBookBtn.addEventListener('click', () => {
+    // Display modal dialog box on clicking "New Book" button
     dialog.showModal();
 });
 
 confirmBtn.addEventListener('click', (e) => {
+    // Handle user pressing "Confirm" within dialog box
+
     // Prevent default form submission
     e.preventDefault();
 
+    // Instantiate new Book object with provided data
     const {title, author, pages, read} = dialogForm.elements;
-
     const newBook = new Book(
         title.value,
         author.value,
@@ -104,6 +122,7 @@ cancelBtn.addEventListener('click', () => {
     dialog.close();
 });
 
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -111,15 +130,11 @@ function Book(title, author, pages, read) {
         // Throw error if instance is declared without using 'new' keyword
         throw Error(`Must use the 'new' operator to call the function`);
     }
+
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-}
-
-Book.prototype.info = function() {
-    const readString = this.read ? 'already read' : 'not read yet';
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${readString}`;
 }
 
 Book.prototype.toggleReadStatus = function () {
@@ -134,6 +149,7 @@ function removeBookFromLibrary(bookIndex) {
     myLibrary.splice(bookIndex, 1);
 }
 
+// Mock objects
 const book1 = new Book(
     'To Kill A Mockingbird',
     'Harper Lee',
